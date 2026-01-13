e3e94213d18d83c69669bf7046bcf2f8de6e04e8
Denis Santos <denisdiogo34@gmail.com>
2026-01-13 08:23:45 -0300

    Criando a Entity User e os campos + colunas na tabela da base de dados.

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  // Criar um construtor com injeção de dependencia do user
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
+}
