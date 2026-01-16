67b7b11c745deaa34c5ae949b35039bc697e976e
Denis Santos <denisdiogo34@gmail.com>
2026-01-16 08:55:04 -0300

    Protegendo rotas com JwtAuthGuard personalizada (class que extendeu AuthGuard e foi modificada) (JWT + NestJS)

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,9 +1,18 @@
-import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { CustomParseIntPipe } from 'src/common/pipes/custom-parse-int-pipe.pipe';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UserService } from './user.service';
-import { AuthGuard } from '@nestjs/passport';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.gard';
+import type { AuthenticatedRequest } from 'src/auth/types/authenticated-request';
 
 @Controller('user')
 export class UserController {
@@ -11,10 +20,16 @@ export class UserController {
     private readonly configService: ConfigService,
     private readonly userService: UserService,
   ) {}
-  @UseGuards(AuthGuard('jwt'))
+  // useguards verifica a estrategia e barra usuários sem altorização acessar a rota que estiver decorada com ele.
+  @UseGuards(JwtAuthGuard)
   @Get(':id') //nomeia tipo de requisição
   //recebe parametros
-  findOne(@Param('id', CustomParseIntPipe) id: number) {
+  findOne(
+    @Req() req: AuthenticatedRequest,
+    @Param('id', CustomParseIntPipe) id: number,
+  ) {
+    console.log(req.user.id);
+    console.log(req.user.email);
     return `olá do controller do user #${id}`;
   }
 
